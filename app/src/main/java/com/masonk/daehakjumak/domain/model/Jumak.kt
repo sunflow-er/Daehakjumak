f10126ebdd7ef6131953f85e0cefd85866c586d0
sunflow-er <antjd03170@gmail.com>
2025-03-31 21:59:12 +0900

    refactor: Domain 레이어 테이블 모델 수정 및 나머지 모델 변경사항 반영

--- app/src/main/java/com/masonk/daehakjumak/domain/model/Jumak.kt
+++ app/src/main/java/com/masonk/daehakjumak/domain/model/Jumak.kt
@@ -2,6 +2,7 @@ package com.masonk.daehakjumak.domain.model
 
 // 주막 정보
 data class Jumak(
+    val id: String, // id
     var name: String, // 주막 이름
     var accountNumber: String, // 계좌번호
     // TODO 추가/수정/보완
--- app/src/main/java/com/masonk/daehakjumak/domain/model/Table.kt
+++ app/src/main/java/com/masonk/daehakjumak/domain/model/Table.kt
@@ -1,9 +1,11 @@
 package com.masonk.daehakjumak.domain.model
 
+import com.masonk.daehakjumak.core.Coordinate
+
 // 테이블
 data class Table(
-    val id: Int, // 테이블 절대 번호 (key/index/id)
-    var tableNumber: Int, // 테이블 상대 번호
-    var isAvailable: Boolean, // 테이블 활성 상태
+    val id: String, // id
+    var number: Int, // 테이블 번호 (테이블 추가 및 삭제에 따라 변함)
+    var coordinate: Coordinate, // 테이블 좌표(x, y), 중심 위치
     val orderList: MutableList<Order>, // 해당 테이블 주문 리스트
 )
