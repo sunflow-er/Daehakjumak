ab222fdaa18078b8401b8321cd56f72850fe9d94
sunflow-er <antjd03170@gmail.com>
2025-05-03 02:27:04 +0900

    feat: 테이블 추가/삭제/좌표 업데이트 관련 TableRepository 항목 추가

--- app/src/main/java/com/masonk/daehakjumak/domain/repo/TableRepository.kt
+++ app/src/main/java/com/masonk/daehakjumak/domain/repo/TableRepository.kt
@@ -1,9 +1,20 @@
 package com.masonk.daehakjumak.domain.repo
 
+import com.masonk.daehakjumak.core.Coordinate
 import com.masonk.daehakjumak.core.dataresource.DataResource
 import com.masonk.daehakjumak.domain.model.Table
 import kotlinx.coroutines.flow.Flow
 
 interface TableRepository {
+    // 테이블 리스트
     fun getTableList(): Flow<DataResource<List<Table>>>
+    
+    // 테이블 추가
+    suspend fun addTable(table: Table): DataResource<Unit>
+
+    // 테이블 삭제
+    suspend fun removeTable(tableId: String): DataResource<Unit>
+
+    // 테이블 위치(죄표) 업데이트
+    suspend fun updateTableCoord(tableId: String, coordinate: Coordinate): DataResource<Unit>
 }
