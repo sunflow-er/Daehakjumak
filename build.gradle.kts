62d55106a528da7c0ff12c3a00234ad6409a5e36
JinVibe <loonaticvibe2.11@gmail.com>
2025-05-09 01:19:20 +0900

    fix: Hilt 의존성 버전 수정

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,6 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
 
-    id("com.google.devtools.ksp")       version "1.9.10-1.0.13" apply false
-    id("com.google.dagger.hilt.android") version "2.56.2" apply false
+    id("com.google.devtools.ksp") version "2.0.0-1.0.23" apply false
+    id("com.google.dagger.hilt.android") version "2.51"  apply false
 }
